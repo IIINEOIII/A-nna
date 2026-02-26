65c29ba9ede30f61105f6a045bcff8b598f3e91a
IIINEOIII <noastranoa@outlook.es>
2026-02-26 20:34:12 -0300

    Mensaje n°1: Subida de archivo Ej1.py

---
+++ Ej1.py
@@ -0,0 +1,9 @@
+#Se define variable nombre.
+nombre=input("Ingrese su nombre: ")
+#Se defien variable materia.
+apellido=input("Ingrese su apellido: ")
+#Se define variable pais.
+pais="Russia"
+#Se imprime mensaje por consola.
+print(f"HOla mi nombre es {nombre} {apellido} y vivo en {pais}")
+   
